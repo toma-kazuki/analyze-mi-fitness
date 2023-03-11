c574d9fe1b995dbf90cda5bc1cfab620b6f63e35
Tatsuya HIRANO <elko7954@gmail.com>
2023-03-11 20:09:51 +0900

    見た目を修正

--- analyze_mi_fitness/Form1.Designer.cs
+++ analyze_mi_fitness/Form1.Designer.cs
@@ -30,36 +30,50 @@
         {
             this.FilePassTextBox = new System.Windows.Forms.TextBox();
             this.SelectFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FilePassTextBox
             // 
-            this.FilePassTextBox.Location = new System.Drawing.Point(38, 23);
-            this.FilePassTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FilePassTextBox.Location = new System.Drawing.Point(13, 70);
+            this.FilePassTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.FilePassTextBox.Name = "FilePassTextBox";
-            this.FilePassTextBox.Size = new System.Drawing.Size(831, 32);
-            this.FilePassTextBox.TabIndex = 0;
+            this.FilePassTextBox.ReadOnly = true;
+            this.FilePassTextBox.Size = new System.Drawing.Size(516, 29);
+            this.FilePassTextBox.TabIndex = 100;
             // 
             // SelectFileButton
             // 
-            this.SelectFileButton.Location = new System.Drawing.Point(380, 65);
-            this.SelectFileButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SelectFileButton.Location = new System.Drawing.Point(537, 64);
+            this.SelectFileButton.Margin = new System.Windows.Forms.Padding(4);
             this.SelectFileButton.Name = "SelectFileButton";
-            this.SelectFileButton.Size = new System.Drawing.Size(161, 59);
-            this.SelectFileButton.TabIndex = 1;
-            this.SelectFileButton.Text = "ファイル選択";
+            this.SelectFileButton.Size = new System.Drawing.Size(53, 40);
+            this.SelectFileButton.TabIndex = 0;
+            this.SelectFileButton.Text = "...";
             this.SelectFileButton.UseVisualStyleBackColor = true;
             this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ランニングデータを選択";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 415);
+            this.ClientSize = new System.Drawing.Size(609, 288);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.SelectFileButton);
             this.Controls.Add(this.FilePassTextBox);
-            this.Font = new System.Drawing.Font("源ノ角ゴシック Code JP R", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Font = new System.Drawing.Font("源ノ角ゴシック Code JP R", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -71,6 +85,7 @@
 
         private System.Windows.Forms.TextBox FilePassTextBox;
         private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
